6da777eca5b57cd8d88fc1461a1dca57cb738a26
anaz <anazibinurasheed@gmail.com>
2024-10-17 06:53:17 +0000

    fixed config issue

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,6 @@ import { store, persistor } from "./redux/store.js";
 import App from "./App.jsx";
 import "./index.css";
 
-alert("Printing...")
-console.log("import.meta.env.VITE_BASE_URL: ",import.meta.env.VITE_BASE_URL)
-console.log("process.env.NODE_ENV: ",process.env.VITE_BASE_URL)
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <Provider store={store}>
