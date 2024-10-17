38856f087bdc2207457d922a640b3e2e78941a79
anaz <anazibinurasheed@gmail.com>
2024-10-17 06:42:59 +0000

    update env

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 alert("Printing...")
 console.log("import.meta.env.VITE_BASE_URL: ",import.meta.env.VITE_BASE_URL)
-console.log("process.env.NODE_ENV: ",process.env.NODE_ENV)
+console.log("process.env.NODE_ENV: ",process.env.VITE_BASE_URL)
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
